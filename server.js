f75070f281896240c3ff83bde60643ec27a71b09
Kobe-An <zhnuowen@163.com>
2020-06-21 18:24:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const createServer = () => {
   const app = express();
   
-  app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal'])
+  app.set('trust proxy', function (ip) {
+  if (ip === '127.0.0.1' || ip === '123.123.123.123') return true // trusted IPs
+  else return false
+})
 
   app.get('/', (req, res, next) => {
     var ipStr = req.headers['X-Real-IP'] || req.headers['x-forwarded-for'];
